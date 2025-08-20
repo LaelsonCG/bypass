1589fe12249b73a9fa076eb3ddaa8a443a05836b
Laelson Coutinho <laelsoncoutinho@gmail.com>
2025-08-20 07:29:07 -0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -8,7 +8,7 @@ export default {
     // URL real de destino (sua VPS/domínio que responde com 101)
     let url = new URL(request.url);
     url.protocol = "wss"; 
-    url.hostname = "tls.clickhost.xyz"; // <- seu DNS que aponta pra VPS
+    url.hostname = "vip.clickhost.xyz"; // <- seu DNS que aponta pra VPS
 
     // repassa a conexão
     return fetch(url.toString(), request);
